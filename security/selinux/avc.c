db9d22abf2f0ee661a38e31a4544b60cf74bdb30
Peppe289 <gsperanza204@protonmail.com>
2024-01-25 15:55:25 +0100

    selinux: fix build without audit
    Signed-off-by: Peppe289 <gsperanza204@protonmail.com>

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -129,6 +129,7 @@ static inline int avc_hash(u32 ssid, u32 tsid, u16 tclass)
 	return (ssid ^ (tsid<<2) ^ (tclass<<4)) & (AVC_CACHE_SLOTS - 1);
 }
 
+#ifdef CONFIG_AUDIT
 /**
  * avc_dump_av - Display an access vector in human-readable form.
  * @tclass: target security class
@@ -197,6 +198,7 @@ static void avc_dump_query(struct audit_buffer *ab, struct selinux_state *state,
 	BUG_ON(!tclass || tclass >= ARRAY_SIZE(secclass_map));
 	audit_log_format(ab, " tclass=%s", secclass_map[tclass-1].name);
 }
+#endif
 
 /**
  * avc_init - Initialize the AVC.
@@ -725,6 +727,7 @@ found:
 	return node;
 }
 
+#ifdef CONFIG_AUDIT
 /**
  * avc_audit_pre_callback - SELinux specific information
  * will be called by generic audit code
@@ -760,6 +763,7 @@ static void avc_audit_post_callback(struct audit_buffer *ab, void *a)
 				 ad->selinux_audit_data->result ? 0 : 1);
 	}
 }
+#endif
 
 /* This is the slow part of avc audit with big stack footprint */
 noinline int slow_avc_audit(struct selinux_state *state,
@@ -797,8 +801,9 @@ noinline int slow_avc_audit(struct selinux_state *state,
 	sad.state = state;
 
 	a->selinux_audit_data = &sad;
-
+#ifdef CONFIG_AUDIT
 	common_lsm_audit(a, avc_audit_pre_callback, avc_audit_post_callback);
+#endif
 	return 0;
 }
 
