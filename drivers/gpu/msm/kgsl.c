c676448819046ed435770f5f1f8f994cc6aa9817
Peppe289 <gsperanza204@protonmail.com>
2024-01-28 17:09:18 +0100

    kgsl: switch to SCHED_RR and increase priority
    Signed-off-by: Peppe289 <gsperanza204@protonmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5248,7 +5248,7 @@ static void kgsl_core_exit(void)
 static int __init kgsl_core_init(void)
 {
 	int result = 0;
-	struct sched_param param = { .sched_priority = 2 };
+	struct sched_param param = { .sched_priority = 99 };
 
 	place_marker("M - DRIVER KGSL Init");
 
@@ -5328,7 +5328,7 @@ static int __init kgsl_core_init(void)
 		goto err;
 	}
 
-	sched_setscheduler(kgsl_driver.worker_thread, SCHED_FIFO, &param);
+	sched_setscheduler(kgsl_driver.worker_thread, SCHED_RR, &param);
 
 	kgsl_events_init();
 
